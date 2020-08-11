afad61901f3b31bfc63160327d45e95e3e1b033c
AlejandroPerdomoHP <alejandro.perdomo.cuello@outlook.com>
2020-08-11 12:23:28 +0200

    added responisveness

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// Function for the Button press
 document.getElementById("btn")
 .addEventListener ("click", function getCityName () {
     let city = document.getElementById("cityName1").value;
     document.getElementById("output").innerHTML = city;
 });
 
+
+// Function for the Enter  Key
 let input = document.getElementById("cityName1");
 input.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
