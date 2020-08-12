4e479de15a6662e07c6c115be0a9067e46cefff1
AlejandroPerdomoHP <alejandro.perdomo.cuello@outlook.com>
2020-08-12 14:11:36 +0200

    polishing the HTML and JS to add the second weather

--- script.js
+++ script.js
@@ -15,20 +15,40 @@ Information on getting the API to work
 
 */
 
+// weather for now with the API already included
 const weatherNow = "api.openweathermap.org/data/2.5/weather?q={city name}&units=metric&appid=50b9caeb9f022d5e06aded241bfda347";
-console.log(weatherNow);
 
+// Now to break the weblink to add the city name
+
+const weatherNow1 = "api.openweathermap.org/data/2.5/weather?q=";
+
+const weatherNow2 = "&units=metric&appid=50b9caeb9f022d5e06aded241bfda347";
+
+// weather for 5 days with the API included
 const weather5 = "api.openweathermap.org/data/2.5/forecast?q={city name}&units=metric&appid=50b9caeb9f022d5e06aded241bfda347";
-console.log(weather5);
 
+// now to break up the weblink to add the city name
 
+const weather51 = "api.openweathermap.org/data/2.5/forecast?q=";
 
-// Function for the Button press
-document.getElementById("btn")
+const weather52 = "&units=metric&appid=50b9caeb9f022d5e06aded241bfda347";
+
+// Function for the Button press that gets the City name from the user
+document.getElementById("mainBtn")
 .addEventListener ("click", function getCityName () {
     let city = document.getElementById("cityName1").value;
-    document.getElementById("output").innerHTML = city;
-    
+    document.getElementById("mainOutput").innerHTML = city;
+
+    // Here we construct the weblink with the proper name
+    let weatherNow3 = weatherNow1 + city + weatherNow2;
+    let weather53 = weather51 + city + weather52;
+
+    // console.log(weatherNow1 + city + weatherNow2);
+    // console.log(weather51 + city + weather52);
+
+    console.log(weatherNow3);
+    console.log(weather53);
+
 });
 
 
@@ -37,6 +57,6 @@ let input = document.getElementById("cityName1");
 input.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
    event.preventDefault();
-   document.getElementById("btn").click();
+   document.getElementById("mainBtn").click();
   }
 });
