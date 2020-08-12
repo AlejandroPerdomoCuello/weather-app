0ab6d124e8a11407026a9137644705d4324aab7b
AlejandroPerdomoHP <alejandro.perdomo.cuello@outlook.com>
2020-08-12 16:03:39 +0200

    fixed the url of the API by adding https://

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const weatherNow = "api.openweathermap.org/data/2.5/weather?q={city name}&units=
 
 // Now to break the weblink to add the city name
 
-const weatherNow1 = "api.openweathermap.org/data/2.5/weather?q=";
+const weatherNow1 = "http://api.openweathermap.org/data/2.5/weather?q=";
 
 const weatherNow2 = "&units=metric&appid=50b9caeb9f022d5e06aded241bfda347";
 
@@ -29,34 +29,48 @@ const weather5 = "api.openweathermap.org/data/2.5/forecast?q={city name}&units=m
 
 // now to break up the weblink to add the city name
 
-const weather51 = "api.openweathermap.org/data/2.5/forecast?q=";
+const weather51 = "http://api.openweathermap.org/data/2.5/forecast?q=";
 
 const weather52 = "&units=metric&appid=50b9caeb9f022d5e06aded241bfda347";
 
 // Function for the Button press that gets the City name from the user
 document.getElementById("mainBtn")
-.addEventListener ("click", function getCityName () {
-    let city = document.getElementById("cityName1").value;
-    document.getElementById("mainOutput").innerHTML = city;
+    .addEventListener("click", function getCityName() {
+        let city = document.getElementById("cityName1").value;
+        document.getElementById("mainOutput").innerHTML = city;
 
-    // Here we construct the weblink with the proper name
-    let weatherNow3 = weatherNow1 + city + weatherNow2;
-    let weather53 = weather51 + city + weather52;
+        // Here we construct the weblink with the proper name
+        let urlNow = weatherNow1 + city + weatherNow2;
+        let url5 = weather51 + city + weather52;
 
-    // console.log(weatherNow1 + city + weatherNow2);
-    // console.log(weather51 + city + weather52);
+        // this is new, so I dont know what I am doing
 
-    console.log(weatherNow3);
-    console.log(weather53);
+        console.log(fetch(urlNow));
 
-});
+
+        
+        fetch(urlNow)
+            .then((response) => {
+                console.log(response);
+                return response.json();
+            })
+            .then(() => {
+                // Work with JSON data here
+            })
+            .catch((err) => {
+                console.log("Error");
+                // Do something for an error here
+            });
+        
+
+    });
 
 
 // Function for the Enter  Key
 let input = document.getElementById("cityName1");
-input.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-   event.preventDefault();
-   document.getElementById("mainBtn").click();
-  }
+input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("mainBtn").click();
+    }
 });
